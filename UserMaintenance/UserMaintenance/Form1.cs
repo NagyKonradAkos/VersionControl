ef6ead8004794c2b983f669a1ff66c3009021ace
NAgyKonradAkos <konrad.nagy2@stud.uni-corvinus.hu>
2022-09-30 16:05:24 +0200

    adduser

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -7,17 +7,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using UserMaintenance.Entities1;
 
 namespace UserMaintenance
 {
     public partial class Form1 : Form
     {
+        BindingList<User1> users = new BindingList<User1>();
         public Form1()
         {
             InitializeComponent();
             lblLastName.Text = Resource2.LastName;
             lblFirstName.Text = Resource2.FirstName;
             btnAdd.Text = Resource2.Add;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            User1 user = new User1();
+            user.FirstName = textBox2.Text;
+            user.LastName = textBox1.Text;
+            users.Add(user);
         }
     }
 }
