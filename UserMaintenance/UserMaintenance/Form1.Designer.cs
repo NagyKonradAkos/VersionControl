d9da24097feae46fc1b16af98287844787c1407f
NAgyKonradAkos <konrad.nagy2@stud.uni-corvinus.hu>
2022-09-30 16:49:13 +0200

    Delete

--- UserMaintenance/UserMaintenance/Form1.Designer.cs
+++ UserMaintenance/UserMaintenance/Form1.Designer.cs
@@ -35,6 +35,7 @@ namespace UserMaintenance
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.btnAdd = new System.Windows.Forms.Button();
             this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // lblLastName
@@ -75,7 +76,7 @@ namespace UserMaintenance
             this.btnAdd.Name = "btnAdd";
             this.btnAdd.Size = new System.Drawing.Size(171, 23);
             this.btnAdd.TabIndex = 4;
-            this.btnAdd.Text = "button1";
+            this.btnAdd.Text = "Add";
             this.btnAdd.UseVisualStyleBackColor = true;
             this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
             // 
@@ -87,11 +88,22 @@ namespace UserMaintenance
             this.listBox1.Size = new System.Drawing.Size(243, 277);
             this.listBox1.TabIndex = 5;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(305, 140);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(171, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Delete";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.btnAdd);
             this.Controls.Add(this.textBox2);
@@ -113,6 +125,7 @@ namespace UserMaintenance
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Button btnAdd;
         private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -13,12 +13,15 @@ namespace UserMaintenance
 {
     public partial class Form1 : Form
     {
+        
         BindingList<User1> users = new BindingList<User1>();
         public Form1()
         {
             InitializeComponent();
             lblLastName.Text = Resource2.FullName;
             btnAdd.Text = Resource2.Add;
+            listBox1.DisplayMember = "FullName";
+            List();
         }
 
         private void btnAdd_Click(object sender, EventArgs e)
@@ -26,6 +29,19 @@ namespace UserMaintenance
             User1 user = new User1();
             user.FullName = textBox1.Text;
             users.Add(user);
+            List();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            users.Remove((User1)listBox1.SelectedItem);
+            List();
+
+        }
+
+        private void List()
+        {
+            listBox1.DataSource = users.ToList();
         }
     }
 }
