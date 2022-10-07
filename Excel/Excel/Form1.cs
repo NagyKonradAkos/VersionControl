ef475250c32a9c154e2a605864af0fb597b1df6c
NAgyKonradAkos <konrad.nagy2@stud.uni-corvinus.hu>
2022-10-07 19:59:53 +0200

    segédfv

--- Excel/Excel/Form1.cs
+++ Excel/Excel/Form1.cs
@@ -70,6 +70,23 @@ namespace Excel
             }
         }
 
+        private string GetCell(int x, int y)
+        {
+            string ExcelCoordinate = "";
+            int dividend = y;
+            int modulo;
+
+            while (dividend > 0)
+            {
+                modulo = (dividend - 1) % 26;
+                ExcelCoordinate = Convert.ToChar(65 + modulo).ToString() + ExcelCoordinate;
+                dividend = (int)((dividend - modulo) / 26);
+            }
+            ExcelCoordinate += x.ToString();
+
+            return ExcelCoordinate;
+        }
+
         void CreateExcel()
         {
             try
@@ -93,5 +110,7 @@ namespace Excel
                 xlApp = null;
             }
         }
+
+
     }
 }
