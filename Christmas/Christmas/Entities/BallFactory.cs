636e23232aa437762d680af68dd77e05df834d3b
i8kk2r <konrad.nagy2@stud.uni-corvinus.hu>
2022-11-04 10:42:50 +0100

    2

---
+++ Christmas/Christmas/Entities/BallFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Christmas.Entities
+{
+    public class BallFactory
+    {
+        public BallFactory()
+        {
+
+        }
+
+        public Ball CreteNew()
+        {
+            return new Ball();
+        }
+    }
+}
