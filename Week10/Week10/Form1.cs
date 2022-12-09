f70ca5ab17a8d307dcea584d13f325df11f3863f
i8kk2r <konrad.nagy2@stud.uni-corvinus.hu>
2022-12-09 10:33:18 +0100

    gameover

--- Week10/Week10/Form1.cs
+++ Week10/Week10/Form1.cs
@@ -28,12 +28,22 @@ namespace Week10
             ga = gc.ActivateDisplay();
             this.Controls.Add(ga);
 
+            gc.GameOver += Gc_GameOver;
+
             for (int i = 0; i < populationSize; i++)
             {
                 gc.AddPlayer(nbrOfSteps);
             }
             gc.Start();
 
+        }
+
+        private void Gc_GameOver(object sender)
+        {
+            generation++;
+            label1.Text = string.Format(
+                "{0}. generáció",
+                generation);
         }
     }
 }
